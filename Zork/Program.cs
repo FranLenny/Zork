9b80165235b051a2a39a954343ebef4b8708995c
FranLenny <42728905+FranLenny@users.noreply.github.com>
2019-09-11 14:53:44 -0400

    Finding Your Direction 3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -13,41 +13,6 @@ namespace Zork
             Console.WriteLine(command);
         }
 
-        private static Commands ToCommand(string commandString)
-        {
-            Commands command;
-            switch (commandString)
-            {
-                case "QUIT":
-                    command = Commands.QUIT;
-                    break;
-
-                case "LOOK":
-                    command = Commands.LOOK;
-                    break;
-
-                case "NORTH":
-                    command = Commands.NORTH;
-                    break;
-
-                case "SOUTH":
-                    command = Commands.SOUTH;
-                    break;
-
-                case "EAST":
-                    command = Commands.EAST;
-                    break;
-
-                case "WEST":
-                    command = Commands.WEST;
-                    break;
-
-                default:
-                    command = Commands.UNKNOWN;
-            }
-            
-
-            return command;
-        }
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
 }
